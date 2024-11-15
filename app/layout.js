6930914614c3e54132f2c4ec0148245575949490
mikle1978-mo <azimuth-m@yandex.ru>
2024-11-15 12:32:49 +0300

    241115-ver.0.0.0.5-analitics

--- app/layout.js
+++ app/layout.js
@@ -83,6 +83,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang='en'>
+            <head>
+                {/* Add Google Analytics script */}
+                <script
+                    async
+                    src='https://www.googletagmanager.com/gtag/js?id=G-N772FVWY5R'
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-N772FVWY5R');
+                        `,
+                    }}
+                />
+            </head>
             <body className={`${geistSans.variable} ${geistMono.variable}`}>
                 <Header />
                 <div className='container'>{children}</div>
