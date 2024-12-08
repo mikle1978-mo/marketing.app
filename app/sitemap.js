c4ba65029611560892f24cce498639f27bffd7c1
mikle1978-mo <azimuth-m@yandex.ru>
2024-12-08 17:25:33 +0300

    241208-ver.1.0.5-sitemap.

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,17 @@
 import { ServicesList } from "@/lib/services";
+import { ArticlesList } from "@/lib/articles";
 
 export default async function sitemap() {
     try {
         // Создание записей для основных страниц услуг
         const serviceEntries = ServicesList.map(({ slug }) => ({
             url: `${process.env.API_URL}/${slug}`,
+            lastModified: new Date().toISOString(),
+        }));
+
+        // Создание записей для страниц блога
+        const blogEntries = ArticlesList.map(({ id }) => ({
+            url: `${process.env.API_URL}/blog/${id}`,
             lastModified: new Date().toISOString(),
         }));
 
@@ -19,6 +26,7 @@ export default async function sitemap() {
         return [
             ...staticEntries, // Общие страницы
             ...serviceEntries, // Основные страницы услуг
+            ...blogEntries, // Страницы блога
         ];
     } catch (error) {
         console.error("Ошибка при генерации sitemap:", error);
