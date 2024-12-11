702f7500a9bee562d1535cac968c5604f36a1384
mikle1978-mo <azimuth-m@yandex.ru>
2024-12-11 15:16:27 +0300

    241211-schema.org.

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -27,10 +27,50 @@ export async function generateMetadata({ params }) {
         description: item.meta_desc,
         keywords: item?.keywords,
         openGraph: {
-            images: item.img || ["/images/default_item.png"],
+            title: item.title,
+            description: item.meta_desc,
+            url: `${process.env.API_URL}/blog/${item.slug}`,
+            images: item.img ? [item.img] : ["/images/default_item.png"],
+            type: "article",
+        },
+        twitter: {
+            card: "summary_large_image", // Тип карточки Twitter (рекомендуется для статей)
+            title: item.title,
+            description: item.meta_desc,
+            image: item.img || `${process.env.API_URL}/images/default_item.png`,
+            creator: item.author || "Автор не указан", // Имя автора
         },
         alternates: {
             canonical: `${process.env.API_URL}/blog/${item.slug}`,
+        },
+        other: {
+            "application/ld+json": JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "Article",
+                headline: item.title,
+                author: {
+                    "@type": "Person",
+                    name: item.author || "Автор не указан",
+                },
+                publisher: {
+                    "@type": "Organization",
+                    name: "MarketingStark",
+                    logo: {
+                        "@type": "ImageObject",
+                        url: `${process.env.API_URL}/logo.png`,
+                    },
+                },
+                datePublished: item.datePublished,
+                dateModified: item.dateModified,
+                mainEntityOfPage: {
+                    "@type": "WebPage",
+                    "@id": `${process.env.API_URL}/blog/${item.slug}`,
+                },
+                description: item.meta_desc,
+                image:
+                    item.img ||
+                    `${process.env.API_URL}/images/default_item.png`,
+            }),
         },
     };
 }
