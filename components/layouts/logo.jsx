1fe10fe88ae0597e71d0149f0c729b6edb37a800
mikle1978-mo <azimuth-m@yandex.ru>
2025-01-13 01:19:20 +0300

    250112-logo.

--- components/layouts/logo.jsx
+++ components/layouts/logo.jsx
@@ -11,7 +11,16 @@ export default function Logo() {
         width={96}
         height={96}
     /> */}
-            <span className={cl.logo_img}>s</span>
+            <span className={cl.img}>
+                <p className={cl.img2}>
+                    <p className={cl.img3}>
+                        <p className={cl.img4}>
+                            {" "}
+                            <p className={cl.img5}></p>
+                        </p>
+                    </p>
+                </p>
+            </span>
             <span className={cl.logo_title}>Marketing Stark</span>
         </Link>
     );
