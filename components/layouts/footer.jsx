a5501846e658aee2598965c732f10de47a8d94ed
mikle1978-mo <azimuth-m@yandex.ru>
2025-02-19 12:57:17 +0300

    250219_articles

--- components/layouts/footer.jsx
+++ components/layouts/footer.jsx
@@ -4,12 +4,6 @@ import cl from "./footer.module.css";
 import Link from "next/link";
 import Social from "@/components/UI/social/social";
 import Copyright from "../copyright/copyright";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faFileLines,
-    faClock,
-    faAddressBook,
-} from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
     return (
@@ -17,10 +11,22 @@ export default function Footer() {
             <div className={cl.bottom_line}>
                 <div className={cl.card}>
                     <div className={cl.icons_wrapper}>
-                        <FontAwesomeIcon
+                        {/* <FontAwesomeIcon
                             className={cl.icons}
                             icon={faFileLines}
-                        />
+                        /> */}
+                        <svg
+                            aria-hidden='true'
+                            className={cl.icons}
+                            role='img'
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 384 512'
+                        >
+                            <path
+                                fill='currentColor'
+                                d='M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM112 256l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z'
+                            ></path>
+                        </svg>
                     </div>
                     <div className={cl.front}>
                         <div className={cl.wrapper}>
@@ -59,10 +65,23 @@ export default function Footer() {
                 </div>
                 <div className={cl.card}>
                     <div className={cl.icons_wrapper}>
-                        <FontAwesomeIcon
+                        {/* <FontAwesomeIcon
                             className={cl.icons}
                             icon={faAddressBook}
-                        />
+                        /> */}
+                        <svg
+                            aria-hidden='true'
+                            data-icon='address-book'
+                            className={cl.icons}
+                            role='img'
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 512 512'
+                        >
+                            <path
+                                fill='currentColor'
+                                d='M96 0C60.7 0 32 28.7 32 64l0 384c0 35.3 28.7 64 64 64l288 0c35.3 0 64-28.7 64-64l0-384c0-35.3-28.7-64-64-64L96 0zM208 288l64 0c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zM512 80c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64zM496 192c-8.8 0-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64c0-8.8-7.2-16-16-16zm16 144c0-8.8-7.2-16-16-16s-16 7.2-16 16l0 64c0 8.8 7.2 16 16 16s16-7.2 16-16l0-64z'
+                            ></path>
+                        </svg>
                     </div>
                     <div className={cl.front}>
                         <div className={cl.wrapper}>
@@ -83,8 +102,8 @@ export default function Footer() {
                                 </li>
 
                                 <li>
-                                    <address>
-                                        адрес: Куздере, Кемер, Анталия, Турция
+                                    <address className={cl.address}>
+                                        адрес: Кемер, Анталия, Турция
                                     </address>
                                 </li>
                             </ul>
@@ -93,7 +112,20 @@ export default function Footer() {
                 </div>
                 <div className={cl.card}>
                     <div className={cl.icons_wrapper}>
-                        <FontAwesomeIcon className={cl.icons} icon={faClock} />
+                        {/* <FontAwesomeIcon className={cl.icons} icon={faClock} /> */}
+                        <svg
+                            aria-hidden='true'
+                            data-icon='clock'
+                            className={cl.icons}
+                            role='img'
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 512 512'
+                        >
+                            <path
+                                fill='currentColor'
+                                d='M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z'
+                            ></path>
+                        </svg>
                     </div>
                     <div className={cl.front}>
                         <div className={cl.wrapper}>
