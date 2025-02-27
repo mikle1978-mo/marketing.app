c33b73fef698aae916398a1786cfe12cf3132ce6
mikle1978-mo <azimuth-m@yandex.ru>
2025-02-27 13:46:26 +0300

    250227 google images robots

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,16 @@
 export default function robots() {
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-            disallow: ["/thanks"],
-        },
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: ["/thanks"],
+            },
+            {
+                userAgent: "Googlebot-Image",
+                disallow: ["/images/"],
+            },
+        ],
         sitemap: `${process.env.API_URL}/sitemap.xml`,
     };
 }
