d771ff188f28235085c37f2f7e15d6ec6eabd48c
mikle1978-mo <azimuth-m@yandex.ru>
2025-03-09 13:36:17 +0300

    250309 calculator cover

--- components/(servicepages)/7_calculator/calculator.jsx
+++ components/(servicepages)/7_calculator/calculator.jsx
@@ -57,88 +57,99 @@ const Calculator = ({ works, title, desc }) => {
 
     return (
         <section className='section'>
-            <h2>{title}</h2>
-            <span
-                className={cl.content}
-                dangerouslySetInnerHTML={{ __html: desc }}
-            />
-            <MyButton onClick={toggleOpen}> Рассчитать</MyButton>
-            <MyModal isOpen={isOpen} toggleOpen={toggleOpen}>
-                <div className={cl.calculator_container}>
-                    <h2>Калькулятор</h2>
-                    <div className={cl.total_wrap}>
-                        {totalCost > 0 && (
-                            <div className={cl.total_cost}>
-                                <p>Стоимость:</p>
-                                <p className={cl.total_cost_value}>
-                                    ${totalCost.toFixed(2)}
-                                </p>
+            <div className='container'>
+                <div className={cl.calculator_cover}>
+                    <h2>{title}</h2>
+                    <span
+                        className={cl.content}
+                        dangerouslySetInnerHTML={{ __html: desc }}
+                    />
+                    <MyButton onClick={toggleOpen}> Рассчитать</MyButton>
+                    <MyModal isOpen={isOpen} toggleOpen={toggleOpen}>
+                        <div className={cl.calculator_container}>
+                            <h2>Калькулятор</h2>
+                            <div className={cl.total_wrap}>
+                                {totalCost > 0 && (
+                                    <div className={cl.total_cost}>
+                                        <p>Стоимость:</p>
+                                        <p className={cl.total_cost_value}>
+                                            ${totalCost.toFixed(2)}
+                                        </p>
+                                    </div>
+                                )}
                             </div>
-                        )}
-                    </div>
-                    <div className={cl.services_list}>
-                        <h3>Общие работы</h3>
-                        {services
-                            .filter((service) => !service.perCampaign)
-                            .map((service) => (
-                                <WorkItem
-                                    key={service.id}
-                                    service={service}
-                                    onToggle={toggleService}
-                                />
-                            ))}
-                        <h3>Работы за каждую кампанию</h3>
-                        <div className={cl.input_group}>
-                            <label htmlFor='campaignCount'>
-                                Количество кампаний:
-                            </label>
-                            <div className={cl.input_with_buttons}>
-                                <button
-                                    className={cl.decrement_button}
-                                    onClick={() =>
-                                        setCampaignCount(
-                                            Math.max(0, campaignCount - 1)
-                                        )
-                                    }
-                                >
-                                    -
-                                </button>
-                                <input
-                                    type='number'
-                                    id='campaignCount'
-                                    value={campaignCount}
-                                    onChange={(e) =>
-                                        setCampaignCount(
-                                            Math.max(
-                                                0,
-                                                parseInt(e.target.value) || 0
-                                            )
-                                        )
-                                    }
-                                    placeholder='Введите количество'
-                                />
-                                <button
-                                    className={cl.increment_button}
-                                    onClick={() =>
-                                        setCampaignCount(campaignCount + 1)
-                                    }
-                                >
-                                    +
-                                </button>
+                            <div className={cl.services_list}>
+                                <h3>Общие работы</h3>
+                                {services
+                                    .filter((service) => !service.perCampaign)
+                                    .map((service) => (
+                                        <WorkItem
+                                            key={service.id}
+                                            service={service}
+                                            onToggle={toggleService}
+                                        />
+                                    ))}
+                                <h3>Работы за каждую кампанию</h3>
+                                <div className={cl.input_group}>
+                                    <label htmlFor='campaignCount'>
+                                        Количество кампаний:
+                                    </label>
+                                    <div className={cl.input_with_buttons}>
+                                        <button
+                                            className={cl.decrement_button}
+                                            onClick={() =>
+                                                setCampaignCount(
+                                                    Math.max(
+                                                        0,
+                                                        campaignCount - 1
+                                                    )
+                                                )
+                                            }
+                                        >
+                                            -
+                                        </button>
+                                        <input
+                                            type='number'
+                                            id='campaignCount'
+                                            value={campaignCount}
+                                            onChange={(e) =>
+                                                setCampaignCount(
+                                                    Math.max(
+                                                        0,
+                                                        parseInt(
+                                                            e.target.value
+                                                        ) || 0
+                                                    )
+                                                )
+                                            }
+                                            placeholder='Введите количество'
+                                        />
+                                        <button
+                                            className={cl.increment_button}
+                                            onClick={() =>
+                                                setCampaignCount(
+                                                    campaignCount + 1
+                                                )
+                                            }
+                                        >
+                                            +
+                                        </button>
+                                    </div>
+                                </div>
+                                {services
+                                    .filter((service) => service.perCampaign)
+                                    .map((service) => (
+                                        <WorkItem
+                                            key={service.id}
+                                            service={service}
+                                            onToggle={toggleService}
+                                        />
+                                    ))}
                             </div>
                         </div>
-                        {services
-                            .filter((service) => service.perCampaign)
-                            .map((service) => (
-                                <WorkItem
-                                    key={service.id}
-                                    service={service}
-                                    onToggle={toggleService}
-                                />
-                            ))}
-                    </div>
+                    </MyModal>
                 </div>
-            </MyModal>
+            </div>
         </section>
     );
 };
